2ebb3a5a634cbfac9638bede3fd5087a265bfc34
9EnAm6 <66507977+9EnAm6@users.noreply.github.com>
2020-06-06 14:52:32 +0600

    Update dmsend.py

--- dmsend.py
+++ dmsend.py
@@ -12,7 +12,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("봇실행이 시작되었습니다(24시간 온라인).")
-    game = discord.Game('~하는 중 ex)봇만들기 하는 중')
+    game = discord.Game('VALORANT')
     await client.change_presence(status=discord.Status.online, activity=game)
 
 #/dm {할말}로 전체DM 전송
@@ -25,13 +25,13 @@ async def on_message(message):
             else:
                 try:
                     msg = message.content[4:]
-                    if message.author.id == 디스코드ID를 적기!!:
-                        embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="최상단 제목")
-                        embed.add_field(name="제목", value=msg, inline=True)
-                        embed.set_footer(text=f"서버초대코드")
+                    if message.author.id == 541975332593336337:
+                        embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="TOXIC-HOMIES2")
+                        embed.add_field(name="TOXIC-HOMIES2", value=msg, inline=True)
+                        embed.set_footer(text=f"https://discord.gg/4388Qha")
                         await i.send(embed=embed)
                 except:
                     pass
 
 
-client.run('봇의 토큰을 넣어주세용')
+client.run('NzE4NzQ4NjA3MjE3NDAxOTg4.XttY4A.DV6MPO3bsx3vXdHmzfQyhzmKHm0')
